074f417444532ffb864b2390a546ec07b9fa034f
Gustavo Mabilia <mabilia.gustavo@gmail.com>
2024-01-21 10:30:21 -0300

    comit inicial

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#insira-telefone').mask('(00) 00000-0000');
+    $('#insira-cpf').mask('000.000.000-00', {reverse: true});
+    $('#insira-CEP').mask('00000-000')
+})
